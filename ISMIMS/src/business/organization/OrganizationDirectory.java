f9c3c03aab9a85e619402c0388528f9c0ff1cec9
GerryNEU <su.guo@northeastern.edu>
2025-12-02 09:55:39 -0500

    Add organization management to OrganizationDirectory
    Introduced organizationList to store organizations and added createOrganization method to instantiate and add organizations based on type. This enhances the OrganizationDirectory with functionality to manage different organization types.

--- ISMIMS/src/business/organization/OrganizationDirectory.java
+++ ISMIMS/src/business/organization/OrganizationDirectory.java
@@ -4,10 +4,59 @@
  */
 package business.organization;
 
+import business.organization.Organization.Type;
+import java.util.ArrayList;
+
 /**
  *
  * @author gerrysu
  */
 public class OrganizationDirectory {
     
+    private ArrayList<Organization> organizationList;
+
+    public OrganizationDirectory() {
+        organizationList = new ArrayList();
+    }
+
+    public ArrayList<Organization> getOrganizationList() {
+        return organizationList;
+    }
+    
+    public Organization createOrganization(Type type){
+        Organization organization = null;
+        if (type.getValue().equals(Type.ApplicationProcessing.getValue())){
+            organization = new ApplicationProcessingOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.InternationalAdmissions.getValue())){
+            organization = new InternationalAdmissionsOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.LegalCompliance.getValue())){
+            organization = new LegalComplianceOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Operations.getValue())){
+            organization = new OperationsOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.FinancialAid.getValue())){
+            organization = new FinancialAidOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Registrar.getValue())){
+            organization = new RegistrarOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.TalentAcquisition.getValue())){
+            organization = new TalentAcquisitionOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.StudentAffairs.getValue())){
+            organization = new StudentAffairsOrganization();
+            organizationList.add(organization);
+        }
+        return organization;
+    }    
 }
