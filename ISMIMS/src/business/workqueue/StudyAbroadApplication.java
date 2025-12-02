fed42b7638df1736624ccd2388aec2885bee457b
GerryNEU <su.guo@northeastern.edu>
2025-12-02 10:51:00 -0500

    Extend StudyAbroadApplication with new fields
    StudyAbroadApplication now extends WorkRequest and includes fields for studentId, major, currentGPA, desiredCountry, selectedUniversity, and result, with corresponding getters and setters. This enhances the application's ability to store and manage study abroad request details.

--- ISMIMS/src/business/workqueue/StudyAbroadApplication.java
+++ ISMIMS/src/business/workqueue/StudyAbroadApplication.java
@@ -8,6 +8,60 @@ package business.workqueue;
  *
  * @author gerrysu
  */
-public class StudyAbroadApplication {
-    
+public class StudyAbroadApplication extends WorkRequest{
+
+    private String studentId;
+    private String major;
+    private double currentGPA;
+    private String desiredCountry;
+    private String selectedUniversity; // The Host University name
+    private String result; // Admission Result
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public double getCurrentGPA() {
+        return currentGPA;
+    }
+
+    public void setCurrentGPA(double currentGPA) {
+        this.currentGPA = currentGPA;
+    }
+
+    public String getDesiredCountry() {
+        return desiredCountry;
+    }
+
+    public void setDesiredCountry(String desiredCountry) {
+        this.desiredCountry = desiredCountry;
+    }
+
+    public String getSelectedUniversity() {
+        return selectedUniversity;
+    }
+
+    public void setSelectedUniversity(String selectedUniversity) {
+        this.selectedUniversity = selectedUniversity;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }    
 }
