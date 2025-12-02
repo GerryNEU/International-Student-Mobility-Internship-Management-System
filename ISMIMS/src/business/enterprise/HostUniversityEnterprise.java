1ef047c9241c7541f74a6f4b083d016fe7baf8e9
GerryNEU <su.guo@northeastern.edu>
2025-12-02 10:15:46 -0500

    Extend enterprise classes and implement getSupportedRole
    HostUniversityEnterprise and VisaGovernmentEnterprise now extend the Enterprise class, implement constructors with enterprise type, and override getSupportedRole to return null. This aligns these classes with the Enterprise hierarchy and prepares them for role-based functionality.

--- ISMIMS/src/business/enterprise/HostUniversityEnterprise.java
+++ ISMIMS/src/business/enterprise/HostUniversityEnterprise.java
@@ -4,10 +4,20 @@
  */
 package business.enterprise;
 
+import business.role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author gerrysu
  */
-public class HostUniversityEnterprise {
+public class HostUniversityEnterprise extends Enterprise{
     
+    public HostUniversityEnterprise(String name){
+        super(name,EnterpriseType.HostUniversity);
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
 }
