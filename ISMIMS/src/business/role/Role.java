bd995de9fded29b21350b9a1c95988cd11bde335
GerryNEU <su.guo@northeastern.edu>
2025-12-02 09:44:33 -0500

    Refactor Role class to abstract and add RoleType enum
    Changed Role to an abstract class, introduced RoleType enum for various roles, and added abstract createWorkArea method. This refactor improves role management and enforces implementation of work area creation for each role.

--- ISMIMS/src/business/role/Role.java
+++ ISMIMS/src/business/role/Role.java
@@ -4,10 +4,53 @@
  */
 package business.role;
 
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.organization.Organization;
+import business.useraccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author gerrysu
  */
-public class Role {
+public abstract class Role {
     
+    public enum RoleType{
+        SystemAdmin("SystemAdmin"),
+        MobilityOfficer("MobilityOfficer"),
+        Student("Student"),
+        AdmissionsOfficer("AdmissionsOfficer"),
+        VisaSpecialist("VisaSpecialist"),
+        LegalVerifier("LegalVerifier"),
+        FinancialAuditor("FinancialAuditor"),
+        Registrar("Registrar"),
+        SiteSupervisor("SiteSupervisor"),
+        CorporateRecruiter("CorporateRecruiter");
+        
+        private String value;
+        private RoleType(String value){
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+    
+    public abstract JPanel createWorkArea(JPanel userProcessContainer, 
+            UserAccount account, 
+            Organization organization, 
+            Enterprise enterprise, 
+            EcoSystem business);
+
+    @Override
+    public String toString() {
+        return this.getClass().getName();
+    }    
 }
