09eb7337b9041e183bd4f8b09345747ba9e07480
GerryNEU <su.guo@northeastern.edu>
2025-12-02 08:47:44 -0500

    Add fields and methods to Employee class
    Introduced name and id fields to the Employee class, along with getter, setter, and toString methods. Also set the main class in project properties and added import statements to EcoSystem.java for future development.

--- ISMIMS/src/business/EcoSystem.java
+++ ISMIMS/src/business/EcoSystem.java
@@ -3,11 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package business;
+import java.util.ArrayList;
+
+import business.network.Network;
+import business.organization.Organization;
+import business.role.Role;
+import business.role.SystemAdminRole;
+import java.util.ArrayList;
 
 /**
  *
  * @author gerrysu
  */
 public class EcoSystem {
-    
+
 }
