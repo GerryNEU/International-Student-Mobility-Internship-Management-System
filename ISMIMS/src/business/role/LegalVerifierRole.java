0bb617a905f8e2f15137ca6d9d02420ce49acd42
GerryNEU <su.guo@northeastern.edu>
2025-12-02 10:33:04 -0500

    Extend LegalVerifierRole and VisaSpecialistRole from Role
    Both LegalVerifierRole and VisaSpecialistRole now extend the Role class and implement the createWorkArea method, returning their respective work area panels. This change enables polymorphic behavior and integration with the application's role-based UI system.

--- ISMIMS/src/business/role/LegalVerifierRole.java
+++ ISMIMS/src/business/role/LegalVerifierRole.java
@@ -4,10 +4,21 @@
  */
 package business.role;
 
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.organization.Organization;
+import business.useraccount.UserAccount;
+import javax.swing.JPanel;
+import ui.LegalVerifierRole.LegalVerifierWorkAreaJPanel;
+
 /**
  *
  * @author gerrysu
  */
-public class LegalVerifierRole {
-    
+public class LegalVerifierRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new LegalVerifierWorkAreaJPanel();
+    }    
 }
--- ISMIMS/src/business/role/VisaSpecialistRole.java
+++ ISMIMS/src/business/role/VisaSpecialistRole.java
@@ -4,10 +4,21 @@
  */
 package business.role;
 
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.organization.Organization;
+import business.useraccount.UserAccount;
+import javax.swing.JPanel;
+import ui.VisaSpecialistRole.VisaSpecialistWorkAreaJPanel;
+
 /**
  *
  * @author gerrysu
  */
-public class VisaSpecialistRole {
-    
+public class VisaSpecialistRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new VisaSpecialistWorkAreaJPanel();
+    }    
 }
