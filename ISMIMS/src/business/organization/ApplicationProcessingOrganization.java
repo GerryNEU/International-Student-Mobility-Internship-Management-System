cf2c188ba4d2f6fb70f9c0617a8c65ecda0290ac
GerryNEU <su.guo@northeastern.edu>
2025-12-02 10:25:59 -0500

    Extend ApplicationProcessingOrganization from Organization
    ApplicationProcessingOrganization now inherits from Organization and implements constructor and getSupportedRole method, adding MobilityOfficerRole support. This change enables role-based functionality for application processing organizations.

--- ISMIMS/src/business/organization/ApplicationProcessingOrganization.java
+++ ISMIMS/src/business/organization/ApplicationProcessingOrganization.java
@@ -4,10 +4,24 @@
  */
 package business.organization;
 
+import business.role.MobilityOfficerRole;
+import business.role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author gerrysu
  */
-public class ApplicationProcessingOrganization {
+public class ApplicationProcessingOrganization extends Organization{
     
+    public ApplicationProcessingOrganization() {
+        super(Organization.Type.ApplicationProcessing.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new MobilityOfficerRole());
+        return roles;
+    }    
 }
