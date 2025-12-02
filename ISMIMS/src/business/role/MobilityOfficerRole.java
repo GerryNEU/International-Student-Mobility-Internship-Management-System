144e805d3beba667aea3cc0145931540dd97ea49
GerryNEU <su.guo@northeastern.edu>
2025-12-02 10:31:38 -0500

    Extend MobilityOfficerRole from Role and add UI method
    MobilityOfficerRole now extends the Role class and implements the createWorkArea method, returning a MobilityOfficerWorkAreaJPanel. This change integrates the role with the application's UI framework.

--- ISMIMS/src/business/role/MobilityOfficerRole.java
+++ ISMIMS/src/business/role/MobilityOfficerRole.java
@@ -4,10 +4,21 @@
  */
 package business.role;
 
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.organization.Organization;
+import business.useraccount.UserAccount;
+import javax.swing.JPanel;
+import ui.MobilityOfficerRole.MobilityOfficerWorkAreaJPanel;
+
 /**
  *
  * @author gerrysu
  */
-public class MobilityOfficerRole {
-    
+public class MobilityOfficerRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new MobilityOfficerWorkAreaJPanel();
+    }    
 }
